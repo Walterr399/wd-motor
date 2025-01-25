cca1115649cadc35236fff1681e5c5c3fa5bd5a0
Walterr399 <walterwhiterp47@gmail.com>
2025-01-25 22:23:25 +0100

    Update client.lua

--- wd-motor/client.lua
+++ wd-motor/client.lua
@@ -4,12 +4,27 @@ AddEventHandler("wd-spawnMotor", function()
     local coords = GetEntityCoords(player)
     local heading = GetEntityHeading(player)
     local vehicleHash = GetHashKey("sanchez")
-    
+
     RequestModel(vehicleHash)
     while not HasModelLoaded(vehicleHash) do
         Wait(100)
     end
-    
-    local vehicle = CreateVehicle(vehicleHash, coords.x, coords.y, coords.z, heading, true, false)
+
+    local vehicle = CreateVehicle(vehicleHash, coords.x, coords.y, coords.z, heading, true, true)
     SetPedIntoVehicle(player, vehicle, -1)
+    SetEntityAsMissionEntity(vehicle, true, true)
+    SetVehicleHasBeenOwnedByPlayer(vehicle, true)
+
+    Citizen.CreateThread(function()
+        while DoesEntityExist(vehicle) do
+            Wait(100)
+            if not IsPedInVehicle(player, vehicle, false) then
+                Wait(1000) 
+                if DoesEntityExist(vehicle) then
+                    DeleteEntity(vehicle)
+                end
+                break
+            end
+        end
+    end)
 end)
